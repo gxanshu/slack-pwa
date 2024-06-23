b8a5575a934abe26740426fec0cf49288d246287
Anshu <gxanshu@yahoo.com>
2024-06-23 14:53:40 +0530

    init

---
+++ src/content.js
@@ -0,0 +1,10 @@
+/**
+ * insert PWA manifest JSON
+ */
+const metaLink = document.createElement("link");
+metaLink.setAttribute("rel", "manifest");
+metaLink.setAttribute(
+  "href",
+  "https://raw.githubusercontent.com/gxanshu/slack-pwa/main/src/pwa/manifest.json"
+);
+document.head.appendChild(metaLink);
